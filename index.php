1411da427eee06a7d78a2608f19a5c3d893fbb24
Tien <dinhtien17082003@gmail.com>
2023-12-05 13:27:24 +0700

    cuoiky

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php 
-	include 'funcition.php';
+	include './function.php';
 	// dùng hàm kiểm tra đăng nhập trong file funciton
 	// nếu đăng nhập rồi thì truy cập vào trang khóa học
 	// còn chưa đăng nhập thì điều hướng ra trang đăng nhập
